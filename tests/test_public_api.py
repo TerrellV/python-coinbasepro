847786517ac53ae16eba59098bd6dfa20d4ced49
Terrell <terrell.vest@gmail.com>
2020-01-27 00:09:29 -0500

    update get asset usd price test cases

--- tests/test_public_api.py
+++ tests/test_public_api.py
@@ -1,6 +1,33 @@
+import pytest
+
+import requests
+
 from coinbase_pro.public_api import CBProPublic
+from coinbase_pro.api import API
 
 
-def test_get_asset_used_price():
-    cb = CBProPublic()
-    assert type(cb.get_asset_usd_price('BTC')) == str
+def test_get_asset_usd_price():
+    # confirm a string is returned
+    # confirm the string can be converted to a float
+    # confirm an exception is raised when an invalid symbol is used
+    sandbox_mode = False
+
+    LIVE_URL = 'https://api.pro.coinbase.com/'
+    SANDBOX_URL = 'https://api-public.sandbox.pro.coinbase.com/'
+
+    base_url = SANDBOX_URL if sandbox_mode else LIVE_URL
+
+    working_symbol = 'eth'
+    unvalid_symbol = '#$@#'
+
+    cb = CBProPublic(sandbox_mode=False)
+    
+    price = cb.get_asset_usd_price(working_symbol)
+
+    assert type(price) == str
+    assert type(float(price)) == float
+    assert price == requests.get(f'{base_url}products/{working_symbol.upper()}-USD/ticker').json()['price']
+
+    with pytest.raises(Exception):
+        cb.get_asset_usd_price(unvalid_symbol)
+    
